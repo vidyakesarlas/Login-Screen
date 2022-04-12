f427e4855bba2dcdc638d8d552a7a787e6986fa1
vidya <vidyask.9704@gmail.com>
2022-04-12 10:03:12 +0530

    with stack view

--- Login Screen/ViewController.swift
+++ Login Screen/ViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        loginButton
+        .layer.cornerRadius = 20    }
 
 
 }
